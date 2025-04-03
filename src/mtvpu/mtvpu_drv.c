b1914a8c58590b8b29e556db01b8b4553ad19cc0
dixyes <dixyes@gmail.com>
2025-04-03 22:57:46 +0800

    Use string for 6.13

--- src/mtvpu/mtvpu_drv.c
+++ src/mtvpu/mtvpu_drv.c
@@ -735,6 +735,8 @@ static struct platform_driver vpu_rpm_core_driver = {
 	.remove = vpu_rpm_core_remove,
 };
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0))
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+MODULE_IMPORT_NS("DMA_BUF");
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0))
 MODULE_IMPORT_NS(DMA_BUF);
 #endif
